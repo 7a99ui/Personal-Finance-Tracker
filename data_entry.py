f5f8e46452acf4fdb403ad1b2ce7a351520516be
haggui akram <akramhaggui2@gmail.com>
2024-10-21 01:36:33 +0100

    ajout de la methode get_amount

--- data_entry.py
+++ data_entry.py
@@ -14,3 +14,13 @@ def get_date(prompt,allow_default=False):
         print("Invalid date format , Please enter date in dd-mm-yyyy format")
         return get_date(prompt,allow_default)
     
+def get_amount():
+    try:
+        amount = float(input("Enter the amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be a non-negative non-zero value.")
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount()    
+    
